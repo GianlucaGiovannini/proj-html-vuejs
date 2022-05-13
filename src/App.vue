886e28e73249ac3bfc8c3aac2ab8e96dae385641
GianlucaGiovannini <gianluca-giova@hotmail.it>
2022-05-13 11:28:39 +0200

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    PROVA 
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style lang="scss">
+  @import '@/assets/scss/style.scss';
+  #app {
+    color: $provaProva;
+  }
+
+</style>
